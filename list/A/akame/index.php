eed47aadce1ea7ab1c727a1ede4acdc2d67d9656
sirojjuddin <sirojjuddina@gmail.com>
2020-07-29 21:55:44 +0700

    Update! akame selesai

--- list/A/akame/index.php
+++ list/A/akame/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="akame.css?v=<?php echo time(); ?>">
-    <title>GGWP</title>
+    <title>Akame Ga Kill! BD</title>
 </head>
 <body>
     <div class="navigasi">
@@ -23,11 +23,45 @@
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="main">
+    <div class="container ">
+        <div class="main cf">
             <h5>Akame ga Kill! BD (Episode 1 -24) Subtittle indonesia</h4>
             <div class="pala">
-                <p>Download Anime ya cuma di sini! di tempat laen ampas!</p>
+                <p><strong>Download Anime ya cuma di sini! di tempat laen ampas!</strong></p>
+            </div>
+            <div class="cover cf">
+                <p><img src="akame.png" alt="">
+                    <table>
+                        <tr><td><strong>Judul</strong></td><td>:</td><td>Akame Ga Kill</td></tr>
+                        <tr><td><strong>Skor</strong></td><td>:</td><td>7.82</td></tr>
+                        <tr><td><strong>Status</strong></td><td>:</td><td>Completed</td></tr>
+                        <tr><td><strong>Durasi</strong></td><td>:</td><td>23 menit</td></tr>
+                        <tr><td><strong>Total Episode</strong></td><td>:</td><td>24</td></tr>
+                        <tr><td><strong>Tanggal Rilis</strong></td><td>:</td><td>juli 7 2014</td></tr>
+                        <tr><td><strong>Genre</strong></td><td>:</td><td>Action, Adventure, Fantasy</td></tr>
+                    </table>
+                    <p class="sinopsis">
+                        Akame ga kill - bercerita tentang seorang pemuda bernama tatsumi yang datang dari desa ke kota
+                        dengan cita-cita suatu hari nanti ia ingin mencari uang dengan menjadi seorang prajurit kerajaan 
+                        dan memakmurkan desanya yang sekarang dilamda kemiskinan dan kelaparan. <br><br>
+                        ia datang ditemani kedua temannya untuk mengikuti ujian masuk menjadi prajurit kerajaan. namun sesampainya disana ia malah tertipu oleh seorang
+                        yang mengaku akan menjadikannya seorang prajurit. <br><br>
+                        Beberapa saat ia di ibu kota, Tatsumi menyadari adanya tipu daya dan kebusukan yang belum banyak 
+                        diketahui di ibu kota tersebut. <br><br>
+                        sayangnya setelah ia tahu bahwa temannya terbunuh tatsumi diajak bergabung oleh sekelompok organisasi misterius bernama night raid.
+                    </p>
+                </p>  
+            </div>
+            <div class="download">
+                <p><strong>Download Akame Ga Kill! Batch!</strong></p>
+            </div>
+            <div class="isi">
+                <div class="ling">
+                    <h6>Download Akame Ga Kill! 720p</h6>
+                    <div class="btn">
+                        <a href="https://drive.google.com/uc?id=1p2ody-QpeoUlmkyvoLW_w3HY0EWqXOwL&confirm=jYel&export=download" target="_blank">Download</a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="footer">
